a9ed2f2770844fb56bbf0ca7930d437050f87d05
sagruvi <nedelkoaroslav@gmail.com>
2024-03-18 17:20:05 +0300

    divided the routes and added protected routes

--- proxy/internal/controller/controller.go
+++ proxy/internal/controller/controller.go
@@ -1,24 +1,91 @@
 package controller
 
 import (
-	"main/proxy/internal/service"
+	"encoding/json"
+	"fmt"
+	"github.com/go-chi/jwtauth"
+	"main/proxy/internal/repository"
+	service "main/proxy/internal/service"
 	"net/http"
 )
 
+type GeoServicer interface {
+	Login(http.ResponseWriter, *http.Request)
+	Register(http.ResponseWriter, *http.Request)
+}
 type Controller struct {
-	Service *service.Service
+	service   *service.Service
+	TokenAuth *jwtauth.JWTAuth
 }
 
-func NewController(service *service.Service) *Controller {
+func NewController(secret string) *Controller {
 	return &Controller{
-		Service: service,
+		service:   service.NewService(repository.NewRepository()),
+		TokenAuth: jwtauth.New("HS256", []byte(secret), nil),
 	}
 }
 
-func (c *Controller) Register(w http.ResponseWriter, r *http.Request) {
-	c.Service.Register(w, r)
+// Login godoc
+// @Summary login users
+// @Description login users using JWT tokens
+// @Tags auth
+// @Accept  json
+// @Produce  json
+// @Param username query string true "Username"
+// @Param password query string true "Password"
+// @Success 200 {string} string "valid JWT token"
+// @Failure 401 {string} string "Unauthorized"
+// @Failure 500 {string} string "error creating token"
+// @Router /login [get]
+func (c *Controller) Login(w http.ResponseWriter, r *http.Request) {
+	var user repository.User
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	if !c.service.CheckUser(user.Username, user.Password) {
+		http.Error(w, "user is not registered", http.StatusUnauthorized)
+		return
+	}
+	_, tokenString, err := c.TokenAuth.
+		Encode(map[string]interface{}{"username": user.Username, "password": user.Password})
+	if err != nil {
+		http.Error(w, "error creating token", http.StatusInternalServerError)
+		return
+	} else {
+		w.Write([]byte("token " + tokenString))
+		w.WriteHeader(http.StatusOK)
+		return
+	}
 }
 
-func (c *Controller) Login(w http.ResponseWriter, r *http.Request) {
-	c.Service.Login(w, r)
+// Register godoc
+// @Summary register users
+// @Description Register users using JWT tokens
+// @Tags auth
+// @Accept  json
+// @Produce  json
+//
+//	@Param			Username and Password				body		string	true	"Username and Password"
+//
+// @Success 200 {string} string "user is registered"
+// @Failure 401 {string} string "error taking a claims"
+// @Failure 500 {string} string "user is already exists"
+// @Failure 500 {string} string "error hashing password"
+// @Router /register [get]
+func (c *Controller) Register(w http.ResponseWriter, r *http.Request) {
+	var user repository.User
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	err = c.service.SaveUser(user.Username, user.Password)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(fmt.Sprintf("user %s is registered: ", user.Username)))
 }
--- proxy/internal/service/service.go
+++ proxy/internal/service/service.go
@@ -1,90 +1,26 @@
 package service
 
 import (
-	"encoding/json"
-	"fmt"
-	"github.com/go-chi/jwtauth"
 	"main/proxy/internal/repository"
-	"net/http"
 )
 
-type GeoServicer interface {
-	Login(http.ResponseWriter, *http.Request)
-	Register(http.ResponseWriter, *http.Request)
-}
 type Service struct {
 	Repository repository.Repository
-	TokenAuth  *jwtauth.JWTAuth
 }
 
-func NewService(secret string) *Service {
+func NewService(repository2 repository.Repository) *Service {
 	return &Service{
-		Repository: repository.NewRepository(),
-		TokenAuth:  jwtauth.New("HS256", []byte(secret), nil),
+		Repository: repository2,
 	}
 }
 
-// Login godoc
-// @Summary login users
-// @Description login users using JWT tokens
-// @Tags auth
-// @Accept  json
-// @Produce  json
-// @Param username query string true "Username"
-// @Param password query string true "Password"
-// @Success 200 {string} string "valid JWT token"
-// @Failure 401 {string} string "Unauthorized"
-// @Failure 500 {string} string "error creating token"
-// @Router /login [get]
-func (s *Service) Login(w http.ResponseWriter, r *http.Request) {
-	var user repository.User
-	err := json.NewDecoder(r.Body).Decode(&user)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-	if !s.Repository.CheckUser(user.Username, user.Password) {
-		http.Error(w, "user is not registered", http.StatusUnauthorized)
-		return
-	}
-	_, tokenString, err := s.TokenAuth.
-		Encode(map[string]interface{}{"username": user.Username, "password": user.Password})
-	if err != nil {
-		http.Error(w, "error creating token", http.StatusInternalServerError)
-		return
-	} else {
-		w.Write([]byte("token " + tokenString))
-		w.WriteHeader(http.StatusOK)
-		return
-	}
+func (s *Service) SaveUser(username, password string) error {
+	return s.Repository.SaveUser(username, password)
 }
 
-// Register godoc
-// @Summary register users
-// @Description Register users using JWT tokens
-// @Tags auth
-// @Accept  json
-// @Produce  json
-//
-//	@Param			Username and Password				body		string	true	"Username and Password"
-//
-// @Success 200 {string} string "user is registered"
-// @Failure 401 {string} string "error taking a claims"
-// @Failure 500 {string} string "user is already exists"
-// @Failure 500 {string} string "error hashing password"
-// @Router /register [get]
-func (s *Service) Register(w http.ResponseWriter, r *http.Request) {
-	var user repository.User
-	err := json.NewDecoder(r.Body).Decode(&user)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-	err = s.Repository.SaveUser(user.Username, user.Password)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(fmt.Sprintf("user %s is registered: ", user.Username)))
+func (s *Service) CheckUser(username, password string) bool {
+	return s.Repository.CheckUser(username, password)
+}
+func (s *Service) CheckPassword(username, password string) bool {
+	return s.Repository.CheckPassword(username, password)
 }
